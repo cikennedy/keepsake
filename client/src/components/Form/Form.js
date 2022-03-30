f0747616f16bc34ba9a5ceaae3dbe881b118c9a7
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-03-30 15:58:32 -0500

    minor aesthetic changes to the form, fixed errors with props

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -19,21 +19,21 @@ const Form = () => {
     }
 
     const clear = () => {
-        
+
     }
 
     return (
         <Paper className={classes.paper}>
-            <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
-                <Typography variant='h6'>Creating a Keepsake</Typography>
-                <TextField name='creator' variant='outlined' label='Creator' fullWidthvalue={postData.creator}
+            <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant='h6'>Create Keepsake</Typography>
+                <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator}
                 // spread postData, meaning in every text field and we do the same thing but only change the last property,
                 // all the data will persist while only changing the specific property
                 onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
                 />
-                <TextField name='title' variant='outlined' label='Title' fullWidthvalue={postData.tags} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
-                <TextField name='message' variant='outlined' label='Message' fullWidthvalue={postData.message} onChange={(e) => setPostData({ ...postData, messsage: e.target.value })}/>
-                <TextField name='tags' variant='outlined' label='Tags' fullWidthvalue={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
+                <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
+                <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, messsage: e.target.value })}/>
+                <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
                 <div className={classes.fileInput}>
                     <FileBase
                         type="file"
@@ -42,7 +42,7 @@ const Form = () => {
                     />
                 </div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-                <Button variant="container" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
         </Paper>
     );
