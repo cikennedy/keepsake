08c276f25cde2ed388c25fa97efa583b1dd5e8a1
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-03-30 13:24:31 -0500

    connected to mongodb cluster with temporary password

--- server/index.js
+++ server/index.js
@@ -2,3 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// connect to mongodb cloud atlas database
+const CONNECTION_URL = 'mongodb+srv://lbkk:K3dcntHDAjHCWJJU@kenken.awtuc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
