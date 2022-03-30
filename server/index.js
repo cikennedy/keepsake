2251209f3944225d52f3ac8e6e5a876eed7cefbf
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-03-30 13:37:47 -0500

    adding initial routing to the application

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+// localhost:5000/posts
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -11,3 +16,10 @@ app.use(cors());
 
 // connect to mongodb cloud atlas database
 const CONNECTION_URL = 'mongodb+srv://lbkk:K3dcntHDAjHCWJJU@kenken.awtuc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
