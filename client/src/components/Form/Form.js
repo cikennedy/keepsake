19e5ede36106fc2beb12e8fe968efe24898ad653
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-01 14:08:37 -0500

    updates to updatePost functionality, changes to form component, post reducers, api

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
 import { createPost } from '../../actions/posts';
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
@@ -14,8 +14,15 @@ const Form = () => {
         tags: '',
         selectedFile: '',
     });
+
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
+
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(post) setPostData(post);
+    }, [post])
 
     // once user submits, sends post request with all the data a user types in
     const handleSubmit = (e) => {
