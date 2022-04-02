ff8998d13d5c3a8a71d802077b0a9d33318f7a0d
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-02 07:10:07 -0500

    destructured mongoose request with spread operator, included post title in the post component

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -34,7 +34,7 @@ export const updatePost = async (req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, id }, { new: true });
 
     res.json(updatedPost);
 }
