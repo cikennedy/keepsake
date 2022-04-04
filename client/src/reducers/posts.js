eb5558dcb07ec6ac065cab9341898a447c4034bd
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-04 16:50:51 -0500

    imported constants for actions/reducers to relevant files and updated for debugging purposes

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,14 +1,16 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+
 // in reducers, state always needs to be equal to something, in this case state=posts
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload);
-        case 'UPDATE':
-        case 'LIKE':    
+        case UPDATE:
+        // case 'LIKE':    
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             // send an array of posts, first spread the posts and add a new post stored in the action payload
             return [...posts, action.payload];
         default:
