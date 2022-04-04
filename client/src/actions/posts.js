fe1c25073d1529a62f99ef746145bccbd002b524
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-04 16:49:00 -0500

    created constants folder for action types with strings in case of any errors or debugging purposes

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 import * as api from '../api';
 
 // action creators are functions that return an action
@@ -6,7 +7,7 @@ export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
 
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
         console.log(error);
     }
@@ -16,7 +17,7 @@ export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
 
-        dispatch({ type: 'CREATE', payload: data });
+        dispatch({ type: CREATE, payload: data });
     } catch (error) {
         console.log(error);
     }
@@ -27,7 +28,7 @@ export const updatePost = (id, post) => async (dispatch) => {
         // destructured response data 
         const { data } = await api.updatePost(id, post);
 
-        dispatch({ type: 'UPDATE', payload: data })
+        dispatch({ type: UPDATE, payload: data })
     } catch (error) {
         console.log(error);
     }
@@ -38,7 +39,7 @@ export const deletePost = (id) => async (dispatch) => {
         // not interested in the return data, only deleting
         await api.deletePost(id);
 
-        dispatch({ type: 'DELETE', payload: id });
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error);
     }
@@ -48,7 +49,7 @@ export const likePost = (id) => async (dispatch) => {
     try {
         const { data } = await api.likePost(id);
 
-        dispatch({ type: 'UPDATE', payload: data });
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(error);
     }
