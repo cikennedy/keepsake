61863d5291b9ae3a58bc93df2e97db7a928b6554
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-04 17:02:56 -0500

    .env file updates

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -5,8 +5,7 @@ export default (posts = [], action) => {
     switch (action.type) {
         case DELETE:
             return posts.filter((post) => post._id !== action.payload);
-        case UPDATE:
-        // case 'LIKE':    
+        case UPDATE:    
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case FETCH_ALL:
             return action.payload;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,10 +17,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 // connect to mongodb cloud atlas database
-const CONNECTION_URL = 'mongodb+srv://lbkk:K3dcntHDAjHCWJJU@kenken.awtuc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://lbkk:K3dcntHDAjHCWJJU@kenken.awtuc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
