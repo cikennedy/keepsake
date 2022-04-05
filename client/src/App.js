71ebac0e1e82ea130496b2f591cd93e06c1a0d54
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-05 12:37:02 -0500

    updates to app and home to remove duplicate components showing

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,17 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 
-const App = () => {
+const App = () => (
+    <BrowserRouter>
+        <Container maxwidth='lg'>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/auth" exact component={Auth} />
+            </Switch>
+        </Container>
+    </BrowserRouter>
+)
 
-    return (
-        <BrowserRouter>
-            <Container maxwidth='lg'>
-                <Navbar />
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/auth" exact component={Auth} />
-                </Switch>
-                <Home />
-            </Container>
-        </BrowserRouter>
-    )
-}
 
 export default App;
