d230b21acbbf551bb13605042263be696b85980a
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-05 07:03:53 -0500

    changes to auth, input components

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, Button, Paper, Grid, Typography, Container } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons";
+
+import useStyles from './styles';
+import Input from "./Input";
 
 const Auth = () => {
-  const state = null;
+  const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
 
-  return <div>Auth</div>;
+  const isSignUp = false;
+
+  const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
+  const handleSubmit = () => {
+
+  };
+
+  const handleChange = () => {
+
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {
+              isSignUp && (
+                <>
+                  <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                  <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+                </>
+              )}
+              <Input name="email" label="Email" handleChange={handleChange} type="email" />
+              <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  )
 };
 
 export default Auth;
