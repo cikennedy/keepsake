87886a03cce5d3983d38e8897928becf6b0305f4
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-05 06:40:14 -0500

    Auth and auth styles updates

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,13 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Avatar, Button, Paper, Grid, Typography, Container } from "@material-ui/core";
 
 const Auth = () => {
-    const state = null;
-    
-  return (
-    <div>
-        Auth
-    </div>
-  )
-}
+  const state = null;
 
-export default Auth
+  return <div>Auth</div>;
+};
+
+export default Auth;
