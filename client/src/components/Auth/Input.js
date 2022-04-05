89d3625f6e48bd7c4c41e52553fc2f9ee6edad3e
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-05 08:15:40 -0500

    proper imports in the Auth and Input components, must fix the like feature

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
 
-import { Visibility } from '@material-ui/icons';
-import { VisibilityOff } from '@material-ui/icons';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPassword }) => {
   return (
