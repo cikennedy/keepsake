5dfab69ae28e60cecae7a28abd9a9861420099cc
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-12 15:38:28 -0500

    changed like functionality, changed corresponding model

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -53,10 +53,22 @@ export const likePost = async (req, res) => {
 
     const { id } = req.params;
 
+    if(!req.userId) return res.json({ message: 'Unauthenticated' });
+
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    // only if the id is not in here is if the index is -1
+    if(index === -1) {
+        post.likes.push(req.userId);
+    } else {
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
 
     res.json(updatedPost);
 
