a9f84b9eea06695595483f991c09c69c53e8fdea
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-12 13:42:09 -0500

    installed jwt, bcrypt, fixed error with file structure

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
-import { signin, signup } from '../controllers/users.js';
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
-router.post('/signin', signin);
-router.post('/signup', signup);
+router.get('/', getPosts);
+router.post('/', createPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
