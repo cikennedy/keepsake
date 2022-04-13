6137942da6229b4a0fecef94f4751c9b4b87c8c9
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-12 19:41:09 -0500

    updates to server side routes for auth

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
-import userRoutes from './routes/users.js';
+import userRoutes from './routes/user.js';
 
 const app = express();
 dotenv.config();
