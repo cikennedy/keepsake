b6a7a0969153b3a0337e94f97571b8416db0991c
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-13 10:51:53 -0500

    resolved error with signup page, .env changes to reflect this

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -15,7 +15,7 @@ export const signin = async (req, res) => {
 
        if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials." });
 
-       const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: '1h' });
+       const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, "test", { expiresIn: '1h' });
 
        res.status(200).json({ result: existingUser, token });
    } catch (error) {
@@ -37,11 +37,13 @@ export const signup = async (req, res) => {
 
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
 
-        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: '1h' });
+        const token = jwt.sign({ email: result.email, id: result._id }, "test", { expiresIn: '1h' });
 
         res.status(200).json({ result, token });
     } catch (error) {
-        res.status(500).json({ message: 'Something went wrong.' });
+        res.status(500).json({ message: error.message });
+
+        console.log(error.message);
     }
     
 }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -9,7 +9,7 @@ const auth = async (req, res, next) => {
         let decodedData;
 
         if(token && isCustomAuth) {
-            decodedData = jwt.verify(token, 'test');
+            decodedData = jwt.verify(token, "test");
 
             req.userId = decodedData?.id;
         } else {
