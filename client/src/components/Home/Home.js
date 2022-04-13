1c0d8c6df87d64780cc015e92b64b44a386212a7
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-13 15:39:41 -0500

    styles and home component changes, search bar added

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -21,6 +21,10 @@ const Home = () => {
     const dispatch = useDispatch();
     const query = useQuery();
     const history = useHistory();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
 
     // once we clear the form, it changes the currentId
     // the app will dispatch the getPosts action
@@ -28,20 +32,53 @@ const Home = () => {
         dispatch(getPosts());
     }, [currentId, dispatch]);
 
+    const handleKeyPress = (e) => {
+      // enter key is 13
+      if(e.keyCode === 13) {
+        // search post 
+      }
+    };
+
+    // when there is an array as a state, must first spread the array and add new 
+    const handleAdd = (tag) => setTags([...tags, tag]);
+
+    const handleDelete = (tagToDelete) => setTags(tags.filter((tag) => tag !== tagToDelete));
+
+    
+
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
-          className={classes.mainContainer}
+          className={classes.gridContainer}
           container
           justifyContent="space-between"
           align-items="stretch"
           spacing={3}
         >
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} md={9} sm={6}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar className={classes.appBar} position="static" color="inherit" >
+                <TextField 
+                name="search" 
+                variant="outlined" 
+                label="Search"
+                onKeyPress={handleKeyPress}
+                fullWidth
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                 />
+                <ChipInput
+                 style={{ margin: '10px 0' }}
+                 value={tags}
+                 onAdd={handleAdd}
+                 onDelete={handleDelete}
+                 label="Search Tags"
+                 variant="outlined"
+                 />
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6} >
               <Pagination />
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -1,23 +1,20 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  appBar: {
-    borderRadius: 15,
-    margin: "30px 0",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+  appBarSearch: {
+    borderRadius: 4,
+    marginBottom: '1rem',
+    display: 'flex',
+    padding: '16px',
   },
-  heading: {
-    color: "rgb(255,20,147)",
+  pagination: {
+    borderRadius: 4,
+    marginTop: '1rem',
+    padding: '16px',
   },
-  image: {
-    marginLeft: "15px",
-  },
-  [theme.breakpoints.down("sm")]: {
-    mainContainer: {
-      flexDirection: "column-reverse",
+  gridContainer: {
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column-reverse',
     },
   },
 }));
