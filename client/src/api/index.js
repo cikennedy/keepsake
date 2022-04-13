7673bc90ae71b9faba393678fa6d56e9b9ef0fc8
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-13 10:12:11 -0500

    api updates to include the request prior to any api calls made

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,8 +4,11 @@ const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
+        // bearer must be prepended to the token
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
     }
+
+    return req;
 });
 
 
