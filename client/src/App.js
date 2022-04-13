02686187867d5605c60ae229fa27b632dbf36020
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-13 14:05:52 -0500

    redirect from auth page if user logged in already

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,10 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 
-const App = () => (
+const App = () => {
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    return (
     <BrowserRouter>
         <Container maxwidth='xl'>
             <Navbar />
@@ -16,11 +19,12 @@ const App = () => (
                 <Route path="/posts" exact component={Home} />
                 <Route path="/posts/search" exact component={Home} />
                 <Route path="/posts/:id" component={PostDetails} />
-                <Route path="/auth" exact component={Auth} />
+                <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
             </Switch>
         </Container>
     </BrowserRouter>
-)
+    )
+}
 
 
 export default App;
