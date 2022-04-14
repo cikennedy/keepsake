e5346ce9e45b0e0a848def26b9fb5079f4b19ea0
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-14 12:25:00 -0500

    reducers updates

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,22 +1,28 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE, FETCH_BY_SEARCH } from '../constants/actionTypes';
 
 // in reducers, state always needs to be equal to something, in this case state=posts
-export default (posts = [], action) => {
+export default (state = [], action) => {
     switch (action.type) {
         case DELETE:
-            return posts.filter((post) => post._id !== action.payload);
+            return state.filter((post) => post._id !== action.payload);
         case UPDATE:
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+            return state.map((post) => post._id === action.payload._id ? action.payload : post);
         case LIKE:    
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+            return state.map((post) => post._id === action.payload._id ? action.payload : post);
         case FETCH_ALL:
-            return action.payload;
+            return {
+                // always spread the state when working with objects
+                ...state,
+                posts: action.payload.data,
+                currentPage: action.payload.currentPage,
+                numberOfPages: action.payload.numberOfPages,
+            };
         case FETCH_BY_SEARCH:
-            return action.payload;
+            return { ...state, posts: action.payload };
         case CREATE:
-            // send an array of posts, first spread the posts and add a new post stored in the action payload
-            return [...posts, action.payload];
+            // send an array of posts, first spread the state and add a new post stored in the action payload
+            return [...state, action.payload];
         default:
-            return posts;
+            return state;
     }
 }
