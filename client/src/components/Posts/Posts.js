90c90c603692dc7b6aca44e7e432bcd1498e754b
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-14 12:29:11 -0500

    dynamic pagination updates

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -9,12 +9,12 @@ import useStyles from './styles';
 const Posts = ({ setCurrentId }) => {
     // in the callback function, get access to state in global index store 
     // posts are named in the reducer 
-    const posts = useSelector((state) => state.posts);
+    const { posts } = useSelector((state) => state.posts);
     const classes = useStyles();
 
     return (
         // if there are no posts, show circular progress
-        !posts.length ? <CircularProgress /> : (
+        !posts?.length ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {posts.map((post) => (
                     <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
