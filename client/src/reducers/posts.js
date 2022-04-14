1a1d95e6c67c9d2a770e9e2069e00709e973b570
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-14 12:07:45 -0500

    navbar styling changes, post reducers added for search functionality

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,4 +1,4 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE, FETCH_BY_SEARCH } from '../constants/actionTypes';
 
 // in reducers, state always needs to be equal to something, in this case state=posts
 export default (posts = [], action) => {
@@ -6,9 +6,12 @@ export default (posts = [], action) => {
         case DELETE:
             return posts.filter((post) => post._id !== action.payload);
         case UPDATE:
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case LIKE:    
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case FETCH_ALL:
+            return action.payload;
+        case FETCH_BY_SEARCH:
             return action.payload;
         case CREATE:
             // send an array of posts, first spread the posts and add a new post stored in the action payload
