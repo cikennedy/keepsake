9b27ecbbb78bd869b894bb090330d076727a5f4a
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-15 14:40:58 -0500

    deployment changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// localhost:5000/posts
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
