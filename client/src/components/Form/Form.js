64c5cf53797ef6b1510c09298ad3be04ffc53849
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-15 08:28:59 -0500

    debugging navbar and form components

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -18,6 +18,7 @@ const Form = ({ currentId, setCurrentId }) => {
     useEffect(() => {
         if(!post?.title) clear();
         if(post) setPostData(post);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [post]);
 
     const clear = () => {
