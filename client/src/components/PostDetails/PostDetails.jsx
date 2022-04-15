77f5818fbf7266e605849656642897d16178773d
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-15 12:17:28 -0500

    individual post, post details imports and changes

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
 
 const PostDetails = () => {
     console.log('POST DETAILS');
